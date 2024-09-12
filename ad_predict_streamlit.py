29793e92799b40296c86095b91ac9909d8eac56e
adarshsahadevan <160005873+adarshsahadevan@users.noreply.github.com>
2024-09-12 13:01:12 +0530

    Update ad_predict_streamlit.py

--- ad_predict_streamlit.py
+++ ad_predict_streamlit.py
@@ -12,7 +12,7 @@ import pandas as pd
 
 
 # Load the saved model
-# ad_predict_model = pickle.load(open("C:/Users/ACER/Softroniics/ML/ML Project (Advertisment)/ad_predict.sav",'rb'))
+ad_predict_model = pickle.load(open(https://github.com/adarshsahadevan/ML-Project-ad-/blob/main/ad_predict.sav))
 
 
 # Load the dataframe (assuming it's a CSV file)
